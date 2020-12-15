64f74c296b7a150d4eaf96f25328506ef96e31c8
mick3 <mvwebber3@gmail.com>
2020-12-15 17:02:33 +0000

    added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,9 +9,19 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-
+    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE /100)
+    bag = request.session.get('bag', {})
     grand_total = delivery + total
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
--- bag/views.py
+++ bag/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
 
 def view_bag(request):
-    """a view to return the bag contents page"""
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
-    """Add a quantity of the specified product to the shopping bag"""
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -22,4 +21,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
