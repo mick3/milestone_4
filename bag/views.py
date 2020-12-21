f7e39a7cb2758983e20facd89f3e4f422878ddf9
mick3 <mvwebber3@gmail.com>
2020-12-21 18:26:06 +0000

    added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -23,3 +23,32 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Add the product amount """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] = quantity
+    else:
+        if quantity > 0:
+            bag[item_id]['quantity'] = quantity
+        else:
+            del bag[item_id]['quantity']
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+    try:
+        remove_from_bag.pop(item_id)
+
+        request.session['remove_from_bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
