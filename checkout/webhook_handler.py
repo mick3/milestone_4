86ced3052dfabf32b3cc694ce8949f39d6d50830
mick3 <mvwebber3@gmail.com>
2020-12-28 20:42:21 +0000

    finished asdd product funtionality

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from django.conf import settings 
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -10,7 +10,6 @@ from profiles.models import UserProfile
 import json
 import time
 
-
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -18,6 +17,7 @@ class StripeWH_Handler:
         self.request = request
 
     def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
@@ -25,12 +25,13 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+        )        
 
     def handle_event(self, event):
         """
@@ -58,20 +59,20 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
-        #Update profile information if save_info is checked
+        # Update profile information if save_info was checked
         profile = None
         username = intent.metadata.username
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
-            if save_info():
-            profile.default_phone_number=shipping_details.phone
-            profile.default_country=shipping_details.address.country
-            profile.default_postcode=shipping_details.address.postal_code
-            profile.default_town_or_city=shipping_details.address.city
-            profile.default_street_address1=shipping_details.address.line1
-            profile.default_street_address2=shipping_details.address.line2
-            profile.default_county=shipping_details.address.state
-            profile.save()
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
