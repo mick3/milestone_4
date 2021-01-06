a26e3ecb04b244a8319c11df83f5f9b1b5f9e802
mick3 <mvwebber3@gmail.com>
2021-01-06 18:58:01 +0000

    added cache control in settings.py

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -177,6 +177,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'milestone-4-michael-webber'
     AWS_S3_REGION_NAME = 'US East (Ohio) us-east-2'
