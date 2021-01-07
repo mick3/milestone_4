d971013d89de9410053a56ece81e8f8b1fe4da7b
mick3 <mvwebber3@gmail.com>
2021-01-07 19:35:26 +0000

    changed AWS_S3_REGION_NAME to 'us-east-1'

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -186,7 +186,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'milestone-4-michael-webber'
-    AWS_S3_REGION_NAME = 'US East (Ohio) us-east-2'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
