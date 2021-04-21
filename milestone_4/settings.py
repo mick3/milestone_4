6dbe68a4603385b392ff02ea222826373ec983e8
mick3 <mvwebber3@gmail.com>
2021-04-21 00:00:57 +0000

    added stuff to settings file

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     #Other
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -177,6 +178,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'milestone-4-mw'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
 
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
