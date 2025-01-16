793d8882b11cb1433cc79c3c2da0163d239287c1
mick3 <mvwebber3@gmail.com>
2025-01-16 16:27:04 -0500

    Update settings.py
    setting up neon database, changed settings.py file in milestone 4

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from pathlib import Path
 import os
-import dj_database_url
+from dotenv import load_dotenv
+from urllib.parse import urlparse
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,12 +132,19 @@ if 'DATABASE_URL' in os.environ:
 
     }
 else:
-    DATABASES = {
-       'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-       }
-   }
+    # Replace the DATABASES section of your settings.py with this
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+    }
+}
 
 
 # Password validation
